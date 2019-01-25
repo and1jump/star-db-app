87207e900f1a47dfbef6fb87131d641fb6979f1a
Daniil <onejump9@gmail.com>
2019-01-25 16:00:54 +0300

    Сompleted RandomPlanet component. Showed list of characters, click on the item, by clicking on the item - its info is displayed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,12 @@ import PersonDetails from "../PersonDetails";
 import "./app.css";
 
 export default class App extends Component {
+  state = { selectedPerson: null };
+
+  onPersonSelected = id => {
+    this.setState({ selectedPerson: id });
+  };
+
   render() {
     return (
       <div className="container stardb-app">
@@ -16,10 +22,10 @@ export default class App extends Component {
 
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
       </div>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -46,7 +46,7 @@ export default class SwapiService {
     return item.url.match(idRegExp)[1];
   }
 
-  _transformPlanet(planet) {
+  _transformPlanet = planet => {
     return {
       id: this._extractId(planet),
       name: planet.name,
@@ -54,9 +54,9 @@ export default class SwapiService {
       rotationPeriod: planet.rotation_period,
       diameter: planet.diameter
     };
-  }
+  };
 
-  _transformStarship(starship) {
+  _transformStarship = starship => {
     return {
       id: this._extractId(starship),
       name: starship.name,
@@ -68,9 +68,9 @@ export default class SwapiService {
       passengers: starship.passengers,
       cargoCapacity: starship.cargoCapacity
     };
-  }
+  };
 
-  _transformPerson(person) {
+  _transformPerson = person => {
     return {
       id: this._extractId(person),
       name: person.name,
@@ -78,5 +78,5 @@ export default class SwapiService {
       birthYear: person.birthYear,
       eyeColor: person.eyeColor
     };
-  }
+  };
 }
